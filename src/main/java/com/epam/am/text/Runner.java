9561519cd6796e765cf2a8cdfc8b15512d4e11e0
Alexander Marushka <marushkaalex@gmail.com>
2016-07-26 13:25:14 +0600

    Generic iterator

--- src/main/java/com/epam/am/text/Runner.java
+++ src/main/java/com/epam/am/text/Runner.java
@@ -1,10 +1,12 @@
 package com.epam.am.text;
 
-import com.epam.am.text.entity.Text;
+import com.epam.am.text.entity.*;
 import com.epam.am.text.parser.Parser;
 import com.epam.am.text.parser.RegexParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Iterator;
 
 public class Runner {
     private static final Logger log = LoggerFactory.getLogger(Runner.class);
@@ -15,5 +17,10 @@ public class Runner {
         Text text = parser.parse(Text.class, testString, 0, testString.length());
         log.info(text.toString());
         text.forEach(i -> log.info(i.toString()));
+
+        Iterator<?> iterator = text.iterator(PunctuationSymbol.class);
+        while (iterator.hasNext()) {
+            log.info(iterator.next().toString());
+        }
     }
 }
--- src/main/java/com/epam/am/text/entity/AbstractComposite.java
+++ src/main/java/com/epam/am/text/entity/AbstractComposite.java
@@ -3,7 +3,10 @@ package com.epam.am.text.entity;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public abstract class AbstractComposite<E extends Component> implements Composite<E> {
     private List<E> componentList = new ArrayList<>();
@@ -32,5 +35,30 @@ public abstract class AbstractComposite<E extends Component> implements Composit
     @Override
     public Iterator<E> iterator() {
         return componentList.iterator();
+    }
+
+    public <T extends Component> Iterator<T> iterator(Class<T> clazz) {
+        return flatten().stream()
+                .filter(i -> clazz.isAssignableFrom(i.getClass()))
+                .map(i -> (T) i)
+                .iterator();
+    }
+
+    public List<? super Component> flatten() {
+        List<? super Component> resList = new ArrayList<>();
+        flatten(this, resList);
+        return resList;
+    }
+
+    private void flatten(Iterable<? extends Component> iterable, List<? super Component> resList) {
+        StreamSupport
+                .stream(iterable.spliterator(), false)
+                .forEach(component -> {
+                    resList.add(component);
+                    if (component instanceof Composite) {
+                        Composite composite = (Composite) component;
+                        flatten(composite, resList);
+                    }
+                });
     }
 }
